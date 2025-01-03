ad959654b50c61361f780a9bb7a45444d425dd09
nuve-ra <nuve19ec@cmrit.ac.in>
2025-01-03 15:28:53 -0600

    Fix Router nesting issue and cleanup code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const NavBar = () => {
@@ -11,18 +11,5 @@ const NavBar = () => {
   )
 }
 
-=======
-import React from 'react'
 
-const NavBar = () => {
-  return (
-    <div className="navbar w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg">
-        <p>
-            Admin panel
-        </p>
-        </div>
-  )
-}
-
->>>>>>> 02490f5711d9b1cd2acba67be3e5512409c48b71
-export default NavBar
+export default NavBar;
